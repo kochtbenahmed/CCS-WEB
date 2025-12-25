210a8d4530c50347457cb2992bfd4c5bdce68a97
kochtbenahmed <kochtben@gmail.com>
2025-12-25 12:37:19 +0100

    Create dashboard.php

---
+++ includes/admin/dashboard.php
@@ -0,0 +1,8 @@
+<?php include '../includes/auth.php'; ?>
+
+<h1>Panneau d’administration</h1>
+<ul>
+    <li><a href="produits.php">Gérer les produits</a></li>
+    <li><a href="secteurs.php">Gérer les secteurs</a></li>
+    <li><a href="logout.php">Déconnexion</a></li>
+</ul>
